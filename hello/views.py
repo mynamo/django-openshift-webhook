3d24da7f8a2c2b70574fa3ab35dcf1cb22aa7f8f
Aditi <adkulkar@redhat.com>
2020-06-21 17:05:39 -0700

    New message

--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def homePageView(request):
-    return HttpResponse('Hello my fellow OpenShift enthusiast!')
+    return HttpResponse('Looks like my webhook works!')
 
 
 
